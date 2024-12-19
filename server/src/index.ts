f73f5731358a2569c9515e1ed489259bfbd443a1
Mats Tončik <tnck.dev@gmail.com>
2024-12-19 14:25:54 +0100

    Added authjs

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,35 @@
 // src/index.ts
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+
+import { ExpressAuth } from "@auth/express";
+
+import GitHub from "@auth/express/providers/github";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL, // Frontend URL
+    credentials: true, // Allow cookies
+  })
+);
+
+app.use(
+  "/api/auth/*",
+  ExpressAuth({
+    providers: [GitHub],
+    callbacks: {
+      async redirect({ url, baseUrl }) {
+        return `${process.env.FRONTEND_URL}/learn`;
+      },
+    },
+  })
+);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
